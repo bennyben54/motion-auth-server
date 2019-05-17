7a6a67effdcadfaf17542e1188554e4df87f2965
bennyben54 <bennyben54@gmail.com>
2019-05-17 23:53:58 +0200

    first conf commit

---
+++ src/main/java/com/beo/motionauthserver/MotionAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.beo.motionauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotionAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotionAuthServerApplication.class, args);
+	}
+
+}
