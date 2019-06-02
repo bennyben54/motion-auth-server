60e60051dbfa127880ba2a0820e5ee3f01b84967
bennyben54 <bennyben54@gmail.com>
2019-06-02 17:04:30 +0200

    [CHORE](pwd) pwd encoder was too strong

--- src/main/java/com/beo/motionauthserver/configuration/AuthPasswordEncoder.java
+++ src/main/java/com/beo/motionauthserver/configuration/AuthPasswordEncoder.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 public class AuthPasswordEncoder extends BCryptPasswordEncoder {
 
     public AuthPasswordEncoder() {
-        super(15);
+        super(4);
     }
 
 }
